8af3d112bfef37844aa563cd8185355c44d3dcfc
Fahar Ahnaf Azis <fhrahnv@Fahars-MacBook-Air.local>
2024-07-25 20:05:32 +0700

    Completed duplicateEncoder challenge

---
+++ 3-duplicateEncoder.js
@@ -0,0 +1,36 @@
+/* 
+The goal of this exercise is to convert a string to a new string where each character i
+n the new string is "(" if that character appears only once in the original string, or ")" 
+if that character appears more than once in the original string. Ignore capitalization when 
+determining if a character is a duplicate.
+*/
+
+// Examples
+
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))(("
+
+/*
+Notes
+
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should 
+encode XXX", the "XXX" is the expected result, not the input!
+*/
+
+function duplicateEncode(word) {
+  const charCount = {};
+  const lowerCaseWord = word.toLowerCase();
+  
+  for(let char of lowerCaseWord) {
+    charCount[char] = (charCount[char] || 0) + 1
+  }
+
+  return word
+    .split("")
+    .map((char) => (charCount[char.toLowerCase()] > 1 ? ")" : "("))
+    .join("");
+}
+
+console.log(duplicateEncode("fahar"));
